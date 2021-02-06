3617bbe3925dbf78cd9a1455b8707b8682018356
CHerron42 <connie.lds@herrons.com>
2021-02-06 16:23:31 -0500

    banner

---
+++ lesson5/js/banner.js
@@ -0,0 +1,18 @@
+            
+                let now = new Date();
+    			let dayOfWeek = now.getDay();
+
+    			//Show message based on Holiday or day of the week
+
+    			if dayOfWeek == 6 {
+    				document.getElementById("bulletin").style.display = "block";
+    			}
+
+                else {
+                    document.getElementById("bulletin").style.display = "none";
+                }
+
+                
+
+    			
+			}
