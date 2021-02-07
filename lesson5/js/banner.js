c075cfcc1010c499186c7e8bca57f301484f982c
CHerron42 <connie.lds@herrons.com>
2021-02-06 23:23:16 -0500

    check2

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,13 +1,13 @@
-            window.onload = function toggleBanner() {
+            
                 let now = new Date();
     			let dayOfWeek = now.getDay();
 
-    			//hide message based on Holiday or day of the week
+    			//show message based on Holiday or day of the week
 
-    			if (dayOfWeek != 6) {
-    				document.getElementById("bulletin").classList.toggle("hide");
+    			if (dayOfWeek == 5) {
+    				document.getElementById("bulletin").style.display="block";
     			}
-            }
+            
 
 
                 
