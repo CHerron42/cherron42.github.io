11a6c02ca87af6f99c7bfbbf9074b8070a09af51
CHerron42 <connie.lds@herrons.com>
2021-02-09 17:06:07 -0500

    last check

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -3,7 +3,7 @@ window.onload = function toggleAnnouncement() {
     let d = new Date();
     let q = d.getDay();
 
-    if (q != 1) {
+    if (q != 5) {
         document.getElementById("bulletin").classList.toggle("hide");
     }
 };
