c9fa7e187e653d4954f08f8658ad988e0755a45e
CHerron42 <connie.lds@herrons.com>
2021-02-09 15:15:25 -0500

    test3

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,17 +1,17 @@
-            function announcement() {
+            
                 let n = new Date();
     			let d = n.getDay();
 
     			//show message based on Holiday or day of the week
 
-    			if (d == 0) {
+    			if (d==0) {
     				document.getElementById("bulletin").style.display="block";
                 }
                 
                 else {
                     document.getElementById("bulletin").style.display="none";
     			}
-            }
+            
             
 
 
