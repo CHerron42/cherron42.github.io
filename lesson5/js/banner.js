d0eb56544bd76f23827d825f8dbc7f5b5bea7cc5
CHerron42 <connie.lds@herrons.com>
2021-02-10 11:24:47 -0500

    banner adjustment

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -8,7 +8,7 @@ window.onload = function toggleAnnouncement() {
     let d = new Date();
     let q = d.getDay();
   
-    if (q != 3) {
+    if (q != 5) {
       document.getElementById("pancakes").classList.toggle("hide");
     }
     //A test to make sure it worked
