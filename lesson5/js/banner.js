9cbc7dc67f2c62d640a5e2840ce480aadca30b22
CHerron42 <connie.lds@herrons.com>
2021-02-10 11:26:11 -0500

    final banner script

--- lesson5/js/banner.js
+++ lesson5/js/banner.js
@@ -1,9 +1,5 @@
 window.onload = function toggleAnnouncement() {
-    // a test to see if my function is getting called when the page loads
-    //   console.log("it worked");
-  
-    // a test to see if I can get the element by ID
-    //   console.log(document.getElementById("pancakes").classList);
+   
   
     let d = new Date();
     let q = d.getDay();
@@ -11,8 +7,7 @@ window.onload = function toggleAnnouncement() {
     if (q != 5) {
       document.getElementById("pancakes").classList.toggle("hide");
     }
-    //A test to make sure it worked
-    //   console.log(q);
+    
   };
             
 
