527463e3698e2200582291bd6cef274afa434ded
CHerron42 <connie.lds@herrons.com>
2021-02-13 03:13:09 -0500

    5th check

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,16 +1,16 @@
 		let tempF = parseFloat(document.getElementById('temperature').value);
     	let speed = parseFloat(document.getElementById('windSpeed').value);
 
-            if (tempF <= 50 && speed >= 3) {
+        if (tempF <= 50 && speed >= 3) {
 
-    		    var windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16;
+    		var windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16;
 
                 document.getElementById('displayWindChill').innerHTML = Math.round(windChillFactor).toFixed(0);
             }
+            else {
 
-                else{
-                windChillFactor = "0";
-                document.getElementById('displayWindChill').innerHTML = windChillFactor;
+            windChillFactor = "0";
+            document.getElementById('displayWindChill').innerHTML = windChillFactor;
             }
 
     		    
