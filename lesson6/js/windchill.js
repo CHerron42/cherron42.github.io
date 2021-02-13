0c9e0f422b80687b837aec7d2725f13782bb4f9c
CHerron42 <connie.lds@herrons.com>
2021-02-13 10:54:40 -0500

    equation

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -5,7 +5,7 @@
        
             if (tempF <= 50 && speed >= 3) {
 
-                let windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * Math.pow(speed, 0.16) + 0.4275 * tempF * Math.pow(speed, 0.16);
+                let windChillFactor = 35.74 + 0.6215 * tempF - 35.75 * speed ** 0.16 + 0.4275 * tempF * speed ** 0.16;
 
                  document.getElementById('displayWindChill').innerHTML = Math.round(windChillFactor).toFixed(0) + "℉";
             } else {
