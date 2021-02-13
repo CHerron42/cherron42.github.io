fa81669c0b2764866e053d66a62905d190a50448
CHerron42 <connie.lds@herrons.com>
2021-02-13 02:17:52 -0500

    check

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,24 +1,25 @@
 		let tempF = parseFloat(document.getElementById('temperature').value);
     	let speed = parseFloat(document.getElementById('windSpeed').value);
 
-			
-
-
-		function windChill(tempF,speed) {
-            if((tempF<=50) && (speed>=3)) {
+            if(tempF <= 50 && speed >= 3) {
 
     		    var windChillFactor = Math.round((35.74 + (0.6215 * tempF)) - (35.75 * (Math.pow(speed,0.16))) + (0.4275 * (tempF*(Math.pow(speed,0.16)))));
+
+                document.getElementById('displayWindChill').innerHTML = windChillFactor.toFixed(0);
             }
 
-            else{
-                var windChillFactor = 0;
+                else{
+                windChillFactor = "0";
+                document.getElementById('displayWindChill').innerHTML = windChillFactor;
             }
 
-    		    return windChillFactor;	 
+    		    
 
-		}
+		
     
-        let wc = windChill(tempF,speed);
-        document.getElementById('displayWindChill').textContent = wc;
+        
+        
+     
+				
 
 
