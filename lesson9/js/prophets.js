8dbb08a8ffee45d0ef940d7f3af23a1692a684c6
CHerron42 <connie.lds@herrons.com>
2021-03-05 00:09:31 -0500

    final update

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -9,22 +9,33 @@ fetch(requestURL)
     const prophets = jsonObject['prophets'];
     for (let i = 0; i < prophets.length; i++ ) {
 let card = document.createElement('section');
-let h2 = document.createElement('h2');
-let p = document.createElement('p');
 let image =document.createElement('img');
+let h2 = document.createElement('h2');
+let dob = document.createElement('p');
+let place = document.createElement('p');
+let los = document.createElement('p');
+let dod = document.createElement('p');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-p.textContent =  'Date of Birth:' + ' ' + prophets[i].birthdate + ' ' + 'Death:' + '' + prophets[i].death + ' ' + 'Length:' + '' + prophets[i].length + ' ' +
-'Order:' + ' ' + prophets[i].order + ' ' + 'Birth place:' + ' ' + prophets[i].birthplace + ' ' + 'Children:' + ' ' + prophets[i].numofchildren;
+
 image.setAttribute('src', prophets[i].imageurl);
+h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+dob.textContent = 'Date of Birth:' + ' ' + prophets[i].birthdate;
+place.textContent = 'Birth place:' + ' ' + prophets[i].birthplace;
+los.textContent = 'Length of Service:' + ' ' + prophets[i].length + ' yrs.';
+dod.textContent = 'Date of Death:' + ' ' + prophets[i].death;
 
 card.appendChild(h2);
-card.appendChild(p);
+card.appendChild(dob);
+card.appendChild(place);
+card.appendChild(los);
+card.appendChild(dod);
 card.appendChild(image);
 
 document.querySelector('div.cards').appendChild(card);
 }
   });
+
+
 
   
 
