396bbe73a3a6b3462c30cb9b2ddb671f77a1256a
CHerron42 <connie.lds@herrons.com>
2021-03-14 00:03:31 -0500

    correction

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -13,7 +13,7 @@ fetch(apiURL)
     let temp = parseFloat(document.getElementById("temperature").innerHTML);
     let speed = parseFloat(document.getElementById("speed").innerHTML);
      
-    if (temp <= 50 && speed > 3) {
+    if (temp <= 50 && speed >= 3) {
       let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
     
       wc = document.getElementById("displayWindChill").innerHTML =
